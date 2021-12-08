eb3b254f46d24cdc13dd23dfe4de833796a661ca
WaseemShaikh <wassim.sheikh@gmail.com>
2021-12-08 16:29:58 +0530

    invoice tax report with QR code

---
+++ addons_account/account_invoice_qweb/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Invoice Qweb",
+
+    'summary': """
+        Invoice Qweb Reports
+        """,
+
+    'description': """
+        Invoice Qweb Reports
+    """,
+
+    'author': "Palmate",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account',
+                'l10n_sa_invoice',
+                ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/report_invoice.xml',
+        'views/account_report.xml',
+        'views/account_move_view.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ addons_account/account_invoice_qweb/models/account_move.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    period_of_supply = fields.Char(string='Period of Supply', copy=False)
+
+    # def _get_name_invoice_report(self):
+    #     self.ensure_one()
+    #     return 'account.report_invoice_document'
+
