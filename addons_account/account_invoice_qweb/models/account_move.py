97804c7ea2a9f2af1c27d0753810771236e1d60b
WaseemShaikh <wassim.sheikh@gmail.com>
2021-12-09 17:50:40 +0530

    usd conversion changes

--- addons_account/account_invoice_qweb/models/account_move.py
+++ addons_account/account_invoice_qweb/models/account_move.py
@@ -13,3 +13,17 @@ class AccountMove(models.Model):
     #     self.ensure_one()
     #     return 'account.report_invoice_document'
 
+    def get_usd_amount(self, amount):
+        us_currency = self.env['res.currency'].search([('name','=','USD')], limit=1)
+        if us_currency and (self.currency_id.id != us_currency.id):
+            if amount and us_currency.rate:
+                amount = float(round(amount * us_currency.rate,2))
+        return amount
+
+    def get_sar_amount(self, amount):
+        sar_currency = self.env['res.currency'].search([('name','=','SAR')], limit=1)
+        if sar_currency and (self.currency_id.id != sar_currency.id):
+            if amount and sar_currency.rate:
+                amount = float(round(amount / self.currency_id.rate,2))
+        return amount
+
