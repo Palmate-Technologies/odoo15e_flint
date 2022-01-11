051cbc431a0736c781bb8e25fc0605925b8b0f55
WaseemShaikh <wassim.sheikh@gmail.com>
2022-01-11 21:34:43 +0530

    show SAR in amount fields in both invoices

--- addons_account/account_invoice_qweb/models/account_move.py
+++ addons_account/account_invoice_qweb/models/account_move.py
@@ -22,5 +22,5 @@ class AccountMove(models.Model):
         if sar_currency and (self.currency_id.id != sar_currency.id):
             if amount and sar_currency.rate:
                 amount = float(round(amount / self.currency_id.rate,2))
-        return amount
+        return str(amount)+' '+sar_currency.name
 
