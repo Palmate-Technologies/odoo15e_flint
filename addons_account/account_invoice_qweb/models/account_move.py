0899a5798655d130ad670f9ccf4af45aafcbb685
WaseemShaikh <wassim.sheikh@gmail.com>
2022-01-20 20:24:25 +0530

    decimal places as of currency

--- addons_account/account_invoice_qweb/models/account_move.py
+++ addons_account/account_invoice_qweb/models/account_move.py
@@ -14,14 +14,14 @@ class AccountMove(models.Model):
         us_currency = self.env['res.currency'].search([('name','=','USD')], limit=1)
         if us_currency and (self.currency_id.id != us_currency.id):
             if amount and us_currency.rate:
-                amount = float(round(amount * us_currency.rate,2))
+                amount = float(round(amount * us_currency.rate, us_currency.decimal_places))
         return amount
 
     def get_sar_amount(self, amount):
         sar_currency = self.env['res.currency'].search([('name','=','SAR')], limit=1)
         if sar_currency and (self.currency_id.id != sar_currency.id):
             if amount and sar_currency.rate:
-                amount = float(round(amount / self.currency_id.rate,2))
+                amount = float(round(amount / self.currency_id.rate, sar_currency.decimal_places))
         amount = '{:,.2f}'.format(amount)
         return str(amount)+' '+sar_currency.name
 
