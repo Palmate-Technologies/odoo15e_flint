e297b35a7d92610c75f8c1e0a753e925597c7134
WaseemShaikh <wassim.sheikh@gmail.com>
2022-07-21 13:30:37 +0530

    spcial orvertime and mol overtime changes1

--- addons-hr/hr_payroll_attendance/models/hr_payroll.py
+++ addons-hr/hr_payroll_attendance/models/hr_payroll.py
@@ -1,5 +1,5 @@
 import time
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 from dateutil import relativedelta
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
 from odoo.exceptions import ValidationError
@@ -27,18 +27,24 @@ class HrPayslipAttendance(models.Model):
     number_of_ot_hours = fields.Float(string='Number of Overtime Hours')
     number_of_ded_hours = fields.Float(string='Number of Deduction Hours')
     contract_id = fields.Many2one('hr.contract', string='Contract', required=True,
-        help="The contract for which applied this input")
+                                  help="The contract for which applied this input")
+
 
 class HrPayslipRun(models.Model):
     _inherit = 'hr.payslip.run'
-    
+
     date_start = fields.Date(string='Date From', required=True, readonly=True,
-        states={'draft': [('readonly', False)]}, default=lambda self: self.env['hr.payslip']._get_contract_date_from())
+                             states={'draft': [('readonly', False)]},
+                             default=lambda self: self.env['hr.payslip']._get_contract_date_from())
     date_end = fields.Date(string='Date To', required=True, readonly=True,
-        states={'draft': [('readonly', False)]},
-        default=lambda self: self.env['hr.payslip']._get_contract_date_to())
-    journal_id = fields.Many2one('account.journal', 'Salary Journal', states={'draft': [('readonly', False)]}, readonly=True,
-        required=True, default=lambda self: self.env['account.journal'].search([('name', 'ilike', 'salary')], limit=1))
+                           states={'draft': [('readonly', False)]},
+                           default=lambda self: self.env['hr.payslip']._get_contract_date_to())
+    journal_id = fields.Many2one('account.journal', 'Salary Journal', states={'draft': [('readonly', False)]},
+                                 readonly=True,
+                                 required=True,
+                                 default=lambda self: self.env['account.journal'].search([('name', 'ilike', 'salary')],
+                                                                                         limit=1))
+
 
 class HrPayslip(models.Model):
     _inherit = 'hr.payslip'
@@ -122,7 +128,8 @@ class HrPayslip(models.Model):
         # Remove Existing
         for summary in attendance_summaries:
             for line in summary.manual_line_ids:
-                existing = self.worked_days_line_ids.filtered(lambda x: x.work_entry_type_id.id == line.work_entry_type_id.id)
+                existing = self.worked_days_line_ids.filtered(
+                    lambda x: x.work_entry_type_id.id == line.work_entry_type_id.id)
                 existing.unlink()
 
         # Insert New
@@ -134,7 +141,9 @@ class HrPayslip(models.Model):
                     'work_entry_type_id': line.work_entry_type_id.id,
                     'number_of_days': 0,
                     'number_of_hours': line.no_of_hours,
-                    'calc_rate': float(round((line.work_entry_type_id.is_calculation and line.work_entry_type_id.calc_rate or 0), precision)),
+                    'calc_rate': float(
+                        round((line.work_entry_type_id.is_calculation and line.work_entry_type_id.calc_rate or 0),
+                              precision)),
                 })
 
         self.write({'worked_days_line_ids': [(0, 0, x) for x in lines]})
@@ -153,7 +162,7 @@ class HrPayslip(models.Model):
     #         input_line.write({'amount': old_values.get(input_line.code,0.0) })
     #
     #     return True
-    
+
     def _get_payslip_days(self):
         company_id = self.env.user.company_id.id
         # ir_values = self.env['ir.values']
@@ -164,11 +173,11 @@ class HrPayslip(models.Model):
         payslip_day_to = self.env['ir.config_parameter'].sudo().get_param('payslip_day_to_setting')
 
         return payslip_day_from, payslip_day_to
-    
+
     def _get_contract_date_from(self):
         user_obj = self.env['res.users']
         date_from = time.strftime('%Y-%m-01')
-        
+
         payslip_day_from, payslip_day_to = self._get_payslip_days()
         if payslip_day_from and payslip_day_to:
             if int(payslip_day_from) < int(payslip_day_to):
@@ -176,32 +185,35 @@ class HrPayslip(models.Model):
             else:
                 date_from = str(datetime.now() + relativedelta.relativedelta(months=-1, day=int(payslip_day_from)))[:10]
         return date_from
-    
+
     def _get_contract_date_to(self):
         user_obj = self.env['res.users']
         date_to = str(datetime.now() + relativedelta.relativedelta(months=+1, day=1, days=-1))[:10]
-        
+
         payslip_day_from, payslip_day_to = self._get_payslip_days()
         if payslip_day_from and payslip_day_to:
             date_to = time.strftime('%Y-%m-' + payslip_day_to)
 
         return date_to
 
-
     date_from = fields.Date(string='Date From', readonly=True, required=True,
-        default=_get_contract_date_from, states={'draft': [('readonly', False)]})
+                            default=_get_contract_date_from, states={'draft': [('readonly', False)]})
     date_to = fields.Date(string='Date To', readonly=True, required=True,
-        default=_get_contract_date_to,
-        states={'draft': [('readonly', False)]})
+                          default=_get_contract_date_to,
+                          states={'draft': [('readonly', False)]})
     attendance_line_ids = fields.One2many('hr.payslip.attendance', 'payslip_id',
-        string='Payslip Attendance', copy=True, readonly=True,
-        states={'draft': [('readonly', False)]})
+                                          string='Payslip Attendance', copy=True, readonly=True,
+                                          states={'draft': [('readonly', False)]})
     journal_id = fields.Many2one('account.journal', 'Salary Journal', readonly=True, required=True,
-        states={'draft': [('readonly', False)]}, default=lambda self: self.env['account.journal'].search([('name', 'ilike', 'salary')], limit=1))
-    attendance_summary_ids = fields.One2many('hr.attendance.summary', 'payslip_id', string='Attendance Summary', copy=False)
+                                 states={'draft': [('readonly', False)]},
+                                 default=lambda self: self.env['account.journal'].search([('name', 'ilike', 'salary')],
+                                                                                         limit=1))
+    attendance_summary_ids = fields.One2many('hr.attendance.summary', 'payslip_id', string='Attendance Summary',
+                                             copy=False)
 
     def _get_worked_day_lines(self, domain=None, check_out_of_contract=True):
         res = super(HrPayslip, self)._get_worked_day_lines(domain=None, check_out_of_contract=True)
+        precision = self.env['decimal.precision'].precision_get('Payroll')
         if self.contract_id:
             attendance_summaries = self.attendance_summary_ids
             if not attendance_summaries:
@@ -219,41 +231,95 @@ class HrPayslip(models.Model):
                 # deduction_hours_compute = sum(attendance_summaries.mapped('deduction_hours_compute'))
 
                 overtime_hours_compute = deduction_hours_compute = 0
+                special_ot_hours_compute = mol_ot_hours_compute = 0
                 for sl in attendance_summaries:
-                    overtime_hours_compute += sum(sl.manual_line_ids.filtered(lambda l:l.work_entry_type_id.code=='OT').mapped('no_of_hours'))
-                    deduction_hours_compute += sum(sl.manual_line_ids.filtered(lambda l:l.work_entry_type_id.code=='ABSDED').mapped('no_of_hours'))
+                    overtime_hours_compute += sum(
+                        sl.manual_line_ids.filtered(lambda l: l.work_entry_type_id.code == 'OT').mapped('no_of_hours'))
+                    deduction_hours_compute += sum(
+                        sl.manual_line_ids.filtered(lambda l: l.work_entry_type_id.code == 'ABSDED').mapped(
+                            'no_of_hours'))
 
-                if overtime_hours_compute:
-                    wo_type_id = False
-                    ot_type_ids = sl.manual_line_ids.filtered(lambda l: l.work_entry_type_id.code == 'OT').mapped('work_entry_type_id')
-                    if ot_type_ids:
-                        wo_type_id = ot_type_ids[0].id
-                    attendances = {
-                        'name': _("Overtime Hours"),
-                        'sequence': 10,
-                        'code': 'OT',
-                        'number_of_days': 0.0,
-                        'number_of_hours':overtime_hours_compute,
-                        'work_entry_type_id': wo_type_id,
-                    }
-                    res.append(attendances)
+                    special_ot_hours_compute += sum(
+                        sl.manual_line_ids.filtered(lambda l: l.work_entry_type_id.code == 'SPCLOT').mapped(
+                            'no_of_hours'))
+                    mol_ot_hours_compute += sum(
+                        sl.manual_line_ids.filtered(lambda l: l.work_entry_type_id.code == 'MOLOT').mapped(
+                            'no_of_hours'))
 
-                if deduction_hours_compute:
-                    wo_type_id = False
-                    ot_type_ids = sl.manual_line_ids.filtered(lambda l: l.work_entry_type_id.code == 'ABSDED').mapped('work_entry_type_id')
-                    if ot_type_ids:
-                        wo_type_id = ot_type_ids[0].id
-                    attendances = {
-                        'name': _("Absent/Deduction Hours"),
-                        'sequence': 15,
-                        'code': 'ABSDED',
-                        'number_of_days': 0.0,
-                        'number_of_hours': deduction_hours_compute,
-                        'work_entry_type_id': wo_type_id,
-                    }
-                    res.append(attendances)
+                    if overtime_hours_compute:
+                        wo_type_id, calc_rate = False, 0.0
+                        wo_type_ids = sl.manual_line_ids.filtered(lambda l: l.work_entry_type_id.code == 'OT').mapped(
+                            'work_entry_type_id')
+                        if wo_type_ids:
+                            wo_type_id = wo_type_ids[0].id
+                            calc_rate = float(
+                                round((wo_type_ids[0].is_calculation and wo_type_ids[0].calc_rate or 0), precision))
+                        attendances = {
+                            'name': _("Overtime Hours"),
+                            'sequence': 10,
+                            'code': 'OT',
+                            'number_of_days': 0.0,
+                            'number_of_hours': overtime_hours_compute,
+                            'work_entry_type_id': wo_type_id,
+                            'calc_rate': calc_rate,
+                        }
+                        res.append(attendances)
+
+                    if special_ot_hours_compute:
+                        wo_type_id, calc_rate = False, 0.0
+                        wo_type_ids = sl.manual_line_ids.filtered(
+                            lambda l: l.work_entry_type_id.code == 'SPCLOT').mapped('work_entry_type_id')
+                        if wo_type_ids:
+                            wo_type_id = wo_type_ids[0].id
+                            calc_rate = float(
+                                round((wo_type_ids[0].is_calculation and wo_type_ids[0].calc_rate or 0), precision))
+                        attendances = {
+                            'name': _("Special Overtime Hours"),
+                            'sequence': 10,
+                            'code': 'SPCLOT',
+                            'number_of_days': 0.0,
+                            'number_of_hours': special_ot_hours_compute,
+                            'work_entry_type_id': wo_type_id,
+                            'calc_rate': calc_rate,
+                        }
+                        res.append(attendances)
+
+                    if mol_ot_hours_compute:
+                        wo_type_id, calc_rate = False, 0.0
+                        wo_type_ids = sl.manual_line_ids.filtered(
+                            lambda l: l.work_entry_type_id.code == 'MOLOT').mapped('work_entry_type_id')
+                        if wo_type_ids:
+                            wo_type_id = wo_type_ids[0].id
+                            calc_rate = float(
+                                round((wo_type_ids[0].is_calculation and wo_type_ids[0].calc_rate or 0), precision))
+                        attendances = {
+                            'name': _("MOL Overtime Hours"),
+                            'sequence': 10,
+                            'code': 'MOLOT',
+                            'number_of_days': 0.0,
+                            'number_of_hours': mol_ot_hours_compute,
+                            'work_entry_type_id': wo_type_id,
+                            'calc_rate': calc_rate,
+                        }
+                        res.append(attendances)
+
+                    if deduction_hours_compute:
+                        wo_type_id = False
+                        wo_type_ids = sl.manual_line_ids.filtered(
+                            lambda l: l.work_entry_type_id.code == 'ABSDED').mapped('work_entry_type_id')
+                        if wo_type_ids:
+                            wo_type_id = wo_type_ids[0].id
+                        attendances = {
+                            'name': _("Absent/Deduction Hours"),
+                            'sequence': 15,
+                            'code': 'ABSDED',
+                            'number_of_days': 0.0,
+                            'number_of_hours': deduction_hours_compute,
+                            'work_entry_type_id': wo_type_id,
+                        }
+                        res.append(attendances)
+
         return res
-
 
     # @api.model
     # def get_worked_day_lines(self, contract_ids, date_from, date_to):
@@ -348,9 +414,7 @@ class HrPayslip(models.Model):
         holidays.write({'payslip_status': False})
         attendance_summaries.write({'state': 'draft'})
         return super(HrPayslip, self).unlink()
-        
 
-    
     # @api.multi
     # def onchange_employee_id(self, date_from, date_to, employee_id=False, contract_id=False):
     #     ### Customize: 1. Month name based on to date
@@ -371,6 +435,3 @@ class HrPayslipWorkedDays(models.Model):
     _inherit = 'hr.payslip.worked_days'
 
     calc_rate = fields.Float(string="Overtime Rate")
-
-
-
